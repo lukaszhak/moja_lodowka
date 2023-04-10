eaa8090ec873008c4a491da303a64a09b1dca47e
lukaszhak <lukasz.hak8@gmail.com>
2023-04-10 22:05:45 +0200

    Organise files in weather page

---
+++ lib/features/home/pages/weather_page/widgets/search_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moja_lodowka/features/home/pages/weather_page/cubit/weather_page_cubit.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+class SearchWidget extends StatelessWidget {
+  SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                  hintText: AppLocalizations.of(context)!.city,
+                  enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(0)))),
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 51, 54),
+              ),
+              onPressed: () {
+                context
+                    .read<WeatherPageCubit>()
+                    .getWeatherModel(city: _controller.text);
+              },
+              child: Text(AppLocalizations.of(context)!.check))
+        ],
+      ),
+    );
+  }
+}
